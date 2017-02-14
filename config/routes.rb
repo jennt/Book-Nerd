8e734afc4298fda39cea3ac9eef393c2e089ac46
Jenn Tustin <jenn.tustin@gmail.com>
2017-02-14 17:50:41 -0500

    Remove old /welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :books
-  get 'welcome/index'
+  # get 'welcome/index'
   root 'welcome#index'
 #   Prefix Verb   URI Pattern               Controller#Action
 #    users GET    /users(.:format)          users#index

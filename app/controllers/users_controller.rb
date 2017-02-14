2253fa6e4b2093b2fd5d516397d683b7bf25a9f5
Jenn Tustin <jenn.tustin@gmail.com>
2017-02-14 09:41:48 -0500

    Remove random '>' in books/new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,12 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)#(params.require(:user).permit(:name))
+
     if @user.save
       redirect_to @user
     else

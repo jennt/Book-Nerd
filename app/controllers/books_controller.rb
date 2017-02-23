fdcc0854ec6e6dcccc83a86c6e94dba260f61c8c
Jenn Tustin <jenn.tustin@gmail.com>
2017-02-22 20:49:28 -0500

    Edit destroy method, books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,13 @@ class BooksController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @book = @user.books.find(params[:id])
+    @book.destroy
+    redirect_to user_path(@user)
+  end
+
   private
     def book_params
       params.require(:book).permit(:title, :read, :user_id)
